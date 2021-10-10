5cbdddb7ac576ba1002fa2297e3fcb37f491c797
四月的船长 <1796717865@qq.com>
2021-10-10 10:43:39 +0800

    "IDEA中的第一次提交"

---
+++ src/main/java/com/atguigu/git/IdeaGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author captain
+ * @create 2021-10-1010:38
+ */
+public class IdeaGit {
+    public static void main(String[] args) {
+        System.out.println("第一次IDEA集成Git");
+    }
+}
