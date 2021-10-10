53c6b836dda4dfa7056e0c8ffa479a7161b8c2d3
四月的船长 <1796717865@qq.com>
2021-10-10 10:57:44 +0800

    "冲突测试：在dev分支上修改了代码"

--- src/main/java/com/atguigu/git/IdeaGit.java
+++ src/main/java/com/atguigu/git/IdeaGit.java
@@ -7,5 +7,8 @@ package com.atguigu.git;
 public class IdeaGit {
     public static void main(String[] args) {
         System.out.println("第一次IDEA集成Git");
+        System.out.println("在 dev 分支上修改了代码");
+
+        System.out.println("冲突测试：在 dev 分支上修改了代码");
     }
 }
